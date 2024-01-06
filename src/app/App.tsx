4c2fcf586392ccd67640d2871992d1c1f00ff799
Deniel <denielaz2020@gmail.com>
2024-01-06 16:09:52 +0100

    refactor: moved searchForm in new feature. Added basic error handling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,12 @@
-import { ConfigProvider, Flex, Layout } from "antd";
+import { ConfigProvider, Layout } from "antd";
 import { themeConfig } from "./config/theme/theme";
-import { Input } from "@/shared/ui/Input/Input";
-import { Button } from "@/shared/ui/Button/Button";
+import { SearchForm } from "@/features/SearchRepository";
 
 function App() {
   return (
     <ConfigProvider theme={themeConfig}>
-      <Layout style={{ maxWidth: 1200, margin: "30px auto", background: "none" }}>
-        <Flex align="center">
-          <Input
-            placeholder="Enter a Github repository name or link"
-          />
-          <Button style={{ borderRadius: 100, minHeight: 70, minWidth: 70 }} type="primary">🔍</Button>
-        </Flex>
+      <Layout style={{ maxWidth: 1200, margin: "25px auto", background: "none" }}>
+        <SearchForm />
       </Layout>
     </ConfigProvider>
   );
